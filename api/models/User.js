ab55837599996273829a434976ce6dd4dd622831
pietrop <pietro.passarelli@gmail.com>
2016-03-07 07:51:31 +0100

    made a note on adding some validation to the models

--- api/models/User.js
+++ api/models/User.js
@@ -3,6 +3,7 @@
 *
 * @description :: TODO: You might write a short summary of how this model works and what it represents here.
 * @docs        :: http://sailsjs.org/#!documentation/models
+* TODO : add some validations http://sailsjs.org/documentation/concepts/models-and-orm/validations
 */
 
 module.exports = {
