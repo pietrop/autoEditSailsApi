9db16eca4051ab4f4aa010e18c11f2ad9f2c1d5b
pietrop <pietro.passarelli@gmail.com>
2016-03-08 14:10:49 -0500

    added views, and mock routes and controller to try them

--- api/controllers/ProjectController.js
+++ api/controllers/ProjectController.js
@@ -1,11 +1,47 @@
 /**
- * ProjectController
- *
- * @description :: Server-side logic for managing projects
- * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
- */
+* ProjectController
+*
+* @description :: Server-side logic for managing projects
+* @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+*/
 
 module.exports = {
-	
-};
+	foo: function(req, res) {
+		if (!req.param('id')) {
+			res.status(200);
+			res.view('project/show', {
+				project:{
+					name: 'a test project name',
+					date:"Today",
+					description:"Some test description",
+					papredits:[
+						{
+							name: "Test Paperedit",
+							description: "Test Paperedit description",
+							sections:[]
+						},
+						{
+							name: "Test Paperedit2",
+							description: "Test Paperedit2 description",
+							sections:[]
+						}
+					],
+					transcripts:[
+						{
+							name: "Test Transcript",
+							description: "Test Transcript description",
+							paragraph:[]
+						},
+						{
+							name: "Test Transcript",
+							description: "Test Transcript description",
+							paragraph:[]
+						}
+					]
+				}
+			});
+			// res.json({name: 'a project name'});
+		}
+	}
 
+};
--- test/integration/controllers/AuthController.test.js
+++ test/integration/controllers/AuthController.test.js
@@ -17,9 +17,12 @@ var mockUserOneUpdated = {
                         };
 
 
+//TODO: add a beforeEach to initialise necessary objects before each test (if that is how beforeEach actually works)
+
+
 describe('AuthController', function() {
 
-// User sign/signup/login Page 
+// User sign/signup/login Page
   describe('#signup()', function() {
     it('should allow a user to sign up providing name,lastname, email and pswd', function (done) {
       request(sails.hooks.http.app)
@@ -34,90 +37,86 @@ describe('AuthController', function() {
     });
   });
 
-//GET (?)
-//   describe('#signin()', function() {
-//     //TODO: add before where you create a new user
-//     it('should sign in an existing user', function (done) {
-//       request(sails.hooks.http.app)
-//         .post(""+api+"/signin")
-//         .send({ email:mockUserOne.email, password: mockUserOne.password })
-//         .expect(200, done);
-//     });
-//   });
+// GET (?)
+  describe('#signin()', function() {
+    //TODO: add before where you create a new user
+    it('should sign in an existing user', function (done) {
+      request(sails.hooks.http.app)
+        .post(""+api+"/signin")
+        .send({ email:mockUserOne.email, password: mockUserOne.password })
+        .expect(200, done);
+    });
+  });
+
+//GET
+//TODO: there needs to be a policy to authenticate users before get, update, delete. perhaps using passportjs with sails and policies(?)
+//http://sailsjs.org/documentation/concepts/policies/sails-passport
 //
-// //GET
-// //TODO: there needs to be a policy to authenticate users before get, update, delete. perhaps using passportjs with sails and policies(?)
-// //http://sailsjs.org/documentation/concepts/policies/sails-passport
-// //
-// //https://github.com/tjwebb/sails-auth
-//   describe('#findOne()', function() {
-//     //TODO: add before where you put a new user and get id.
-//     it('should find a user by id', function (done) {
-//       request(sails.hooks.http.app)
-//       //TODO: check if there is an other way to deal with user id number so that is more programmatic and not hard coded.
-//         .get(""+api+"/user/1")
-//         .expect(200, done)
-//         .expect(function(res) {
-//         // res.body.id = 'some fixed id';
-//         // res.body.name = res.body.name.toUpperCase();
-//         should(res.body.name).be.exactly(mockUserOne.name);
-//         should(res.body.lastname).be.exactly(mockUserOne.lastname);
-//         should(res.body.email).be.exactly(mockUserOne.email);
-//         // console.log(res.body);
-//       });
-//     });
-//   });
-//
-// //PUT
-//   describe('#update()', function() {
-//     var userId;
-//
-//       before(function() {
-//       // runs before all tests in this block
-//       //TODO: setup update test, but creating a user, grabbing userId so that it can be used in the retoute to update.
-//       console.log("BEFORE")
-//     });
-//
-//
-//     it('should update a user s attributes', function (done) {
-//       //mockuser to use to update attributes of old one
-//
-//
-//       request(sails.hooks.http.app)
-//       .post(""+api+"/user/2")
-//       .send(mockUserOneUpdated)
-//         .expect(200, done)
-//         .expect(function(res) {
-//         // res.body.id = 'some fixed id';
-//         // res.body.name = res.body.name.toUpperCase();
-//         should(res.body.name).be.exactly(mockUserOneUpdated.name);
-//         should(res.body.lastname).be.exactly(mockUserOneUpdated.lastname);
-//         // console.log(res.body);
-//       });
-//     });
-//   });
-//
-//   describe('#delete()', function() {
-//     it('should update a user s attributes', function (done) {
-//       //mockuser to use to update attributes of old one
-//       var mockUserOneUpdated = {
-//                           name: 'Jack',
-//                           lastname:'Hart'
-//                         };
-//
-//       request(sails.hooks.http.app)
-//       .delete(""+api+"/user/1")
-//       // .send(mockUserOneUpdated)
-//         .expect(200, done)
-//         .expect(function(res) {
-//         // When deleting an object sails blueprint returns the object being deleted
-//         //TODO: perhaps to be changed with a response sucesfull instead?
-//         should(res.body.name).be.exactly(mockUserOne.name);
-//         should(res.body.lastname).be.exactly(mockUserOne.lastname);
-//         // console.log(res.body);
-//       });
-//     });
-//   });
+//https://github.com/tjwebb/sails-auth
+  describe('#findOne()', function() {
+    //TODO: add before where you put a new user and get id.
+    it('should find a user by id', function (done) {
+      request(sails.hooks.http.app)
+      //TODO: check if there is an other way to deal with user id number so that is more programmatic and not hard coded.
+        .get(""+api+"/user/1")
+        .expect(200, done)
+        .expect(function(res) {
+        // res.body.id = 'some fixed id';
+        // res.body.name = res.body.name.toUpperCase();
+        should(res.body.name).be.exactly(mockUserOne.name);
+        should(res.body.lastname).be.exactly(mockUserOne.lastname);
+        should(res.body.email).be.exactly(mockUserOne.email);
+        // console.log(res.body);
+      });
+    });
+  });
+
+//PUT
+  describe('#update()', function() {
+    var userId;
+      before(function() {
+      // runs before all tests in this block
+      //TODO: setup update test, but creating a user, grabbing userId so that it can be used in the retoute to update.
+      console.log("BEFORE")
+    });
+
+    it('should update a user s attributes', function (done) {
+      //mockuser to use to update attributes of old one
+      request(sails.hooks.http.app)
+      .post(""+api+"/user/2")
+      .send(mockUserOneUpdated)
+        .expect(200, done)
+        .expect(function(res) {
+        // res.body.id = 'some fixed id';
+        // res.body.name = res.body.name.toUpperCase();
+        should(res.body.name).be.exactly(mockUserOneUpdated.name);
+        should(res.body.lastname).be.exactly(mockUserOneUpdated.lastname);
+        // console.log(res.body);
+      });
+    });
+  });
+
+  describe('#delete()', function() {
+    it('should update a user s attributes', function (done) {
+      //mockuser to use to update attributes of old one
+      var mockUserOneUpdated = {
+                          name: 'Jack',
+                          lastname:'Hart'
+                        };
+
+      request(sails.hooks.http.app)
+      .delete(""+api+"/user/1")
+      // .send(mockUserOneUpdated)
+        .expect(200, done)
+        .expect(function(res) {
+        // When deleting an object sails blueprint returns the object being deleted
+        //TODO: perhaps to be changed with a response sucesfull instead?
+        should(res.body.name).be.exactly(mockUserOne.name);
+        should(res.body.lastname).be.exactly(mockUserOne.lastname);
+        // console.log(res.body);
+      });
+    });
+  });
 
 
 
