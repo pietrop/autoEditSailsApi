26e38a0bc250ebad878edda162498322a3dc0816
andrixb <andrixb@gmail.com>
2016-03-12 15:45:55 +0000

    auth api integrated

--- api/models/Project.js
+++ api/models/Project.js
@@ -1,9 +1,9 @@
 /**
-* Project.js
-*
-* @description :: TODO: You might write a short summary of how this model works and what it represents here.
-* @docs        :: http://sailsjs.org/#!documentation/models
-*/
+ * Project.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
 
 module.exports = {
 
@@ -35,3 +35,4 @@ module.exports = {
 
   }
 };
+
