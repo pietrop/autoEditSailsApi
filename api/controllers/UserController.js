5189ab4bee19f8846673ab2f94ac16ef25723aa7
andrixb <andrixb@gmail.com>
2016-03-13 23:36:15 +0000

    changed update method

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -8,7 +8,8 @@
  * @docs        :: http://waterlock.ninja/documentation
  */
 
-// var actionUtil = require('../../node_modules/sails/lib/hooks/blueprints/actionUtil');
+var actionUtil = require('../../node_modules/sails/lib/hooks/blueprints/actionUtil');
+var sails = require('sails');
 
 module.exports = require('waterlock').actions.user({
 
@@ -60,21 +61,75 @@ module.exports = require('waterlock').actions.user({
      ================================================================*/
     update: function(req, res) {
 
-        User.update({
-                id: req.session.user.id
-            },
-            req.body,
-            function(err, users) {
-                // Error handling
-                if (err) {
-                    console.log(err);
-                    return res.send(err);
-                    // Updated users successfully!
-                } else {
-                    console.log("Users updated:", users);
-                    return res.json('user updated');
+        // Look up the model
+        var Model = actionUtil.parseModel(req);
+
+        // Locate and validate the required `id` parameter.
+        var pk = { id: req.session.user.id };
+      
+        // Create `values` object (monolithic combination of all parameters)
+        // But omit the blacklisted params (like JSONP callback param, etc.)
+        var values = actionUtil.parseValues(req);
+
+        // Omit the path parameter `id` from values, unless it was explicitly defined
+        // elsewhere (body/query):
+        var idParamExplicitlyIncluded = ((req.body && req.body.id) || req.query.id);
+        if (!idParamExplicitlyIncluded) delete values.id;
+
+
+        // Find and update the targeted record.
+        //
+        // (Note: this could be achieved in a single query, but a separate `findOne`
+        //  is used first to provide a better experience for front-end developers
+        //  integrating with the blueprint API.)
+        Model.findOne(pk).populateAll().exec(function found(err, matchingRecord) {
+
+            if (err) return res.serverError(err);
+            if (!matchingRecord) return res.notFound();
+
+            Model.update(pk, values).exec(function updated(err, records) {
+
+                // Differentiate between waterline-originated validation errors
+                // and serious underlying issues. Respond with badRequest if a
+                // validation error is encountered, w/ validation info.
+                if (err) return res.negotiate(err);
+
+
+                // Because this should only update a single record and update
+                // returns an array, just use the first item.  If more than one
+                // record was returned, something is amiss.
+                if (!records || !records.length || records.length > 1) {
+                    req._sails.log.warn(
+                        util.format('Unexpected output from `%s.update`.', Model.globalId)
+                    );
                 }
-            });
+
+                var updatedRecord = records[0];
+
+                // If we have the pubsub hook, use the Model's publish method
+                // to notify all subscribers about the update.
+                if (req._sails.hooks.pubsub) {
+                    if (req.isSocket) { Model.subscribe(req, records); }
+                    Model.publishUpdate(pk, _.cloneDeep(values), !req.options.mirror && req, {
+                        previous: matchingRecord.toJSON()
+                    });
+                }
+
+                // Do a final query to populate the associations of the record.
+                //
+                // (Note: again, this extra query could be eliminated, but it is
+                //  included by default to provide a better interface for integrating
+                //  front-end developers.)
+                var Q = User.findOne(updatedRecord[User.primaryKey]);
+                Q = actionUtil.populateEach(Q, req);
+                Q.exec(function foundAgain(err, populatedRecord) {
+                    if (err) return res.serverError(err);
+                    if (!populatedRecord) return res.serverError('Could not find record after updating!');
+                    res.ok(populatedRecord);
+                }); // </foundAgain>
+            }); // </updated>
+        }); // </found>
+
     },
 
     /*=====  End of update  ======*/
@@ -91,25 +146,25 @@ module.exports = require('waterlock').actions.user({
             User.destroy({ id: req.session.user.id }).exec(function destroyedRecord(err) {
                 if (err) return res.negotiate(err);
 
-                // if (sails.hooks.pubsub) {
-                //     User.publishDestroy(pk, !sails.config.blueprints.mirror && req, { previous: record });
                 if (req.isSocket) {
                     User.unsubscribe(req, record);
                     User.retire(record);
                 }
-            });
 
+                //======================>>>>>>>
+                req.session.destroy(function(err) {
+                    if (err) return res.negotiate(err);
+                });
+                //======================>>>>>>>
+            });
+            sails.log(req.session.user);
             return res.json({
                 success: 'Deleted user, if it existed.'
             });
         });
     }
 
-
     /*=====  End of delete  ======*/
-
-
-
 
 
 });
