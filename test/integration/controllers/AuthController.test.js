3d705eba398921369e3ea349c401e60c22aa3706
andrixb <andrixb@gmail.com>
2016-03-15 22:58:36 +0000

    implemented logout test in auth contrroller. moved delete test code in user controller test file

--- test/integration/controllers/AuthController.test.js
+++ test/integration/controllers/AuthController.test.js
@@ -5,20 +5,9 @@ var users = require("../../fixtures/users.json")
 
 var mockUserLogin = users[0];
 
-//Uses credentials of user object in fxture folder tha thave been seeded in the database using test bootstrap file
-var mockUserLoginCredentials = {
-   "email":"LongJon@Silver.com",
-   "password":"TreasureIsland"
- }
-
-var mockUserRegister = {
-  "lastname":"Sparrow",
-  "email":"Jack@sparrow.com"
-}
-
 var fakeMockUserLogin = {
-  "email": "fake@fake.com",
-  "password": "FakeUserPswd"
+  "email": "John@Armstrong.com",
+  "password": "MoonLanding"
 };
 
 var mockUserRegister = {
@@ -29,6 +18,7 @@ var mockUserRegister = {
 }
 
 describe('AuthController', function() {
+
   describe('#register()', function() {
     it('should allow a user to register providing name, lastname, email and pswd', function(done) {
       request.agent(sails.hooks.http.app)
@@ -40,41 +30,91 @@ describe('AuthController', function() {
           done(err)
         });
     });
-
   });
 
-  // User sign/signup/login Page
+  // Auth sign/signup/login Page
   describe('#login()', function() {
     it('should allow a user to login up providing  email and pswd', function(done) {
       request(sails.hooks.http.app)
         .post("/auth/login")
-        .send(mockUserRegister)
+        .send(fakeMockUserLogin)
         .expect(200)
         .end(function(err, res) {
-          // console.log(res.body.token)
-          // console.log(res.body.expires)
-          should(res.body.token).be.type('string');///.with.lengthOf(4)
-          should(res.body.expires).be.type('number');
-          done(err)
-        });
-    });
-
-    it('should NOT allow a user to login providing FAKE email and pswd', function(done) {
-      request(sails.hooks.http.app)
-        .post("/auth/login")
-        .send(mockUserLoginCredentials)
-        .expect(403)
-        .end(function(err, res) {
-          // console.log(res.body)
-          should(res.body.error).be.exactly('user not found');
+          // should(res.body.auth.email).be.exactly(mockUserLogin.email);
           done(err)
         });
     });
 
   });
 
+  // Auth logout
+describe('#logout()', function(){
+  it('should logout the user', function(done){
+    request(sails.hooks.http.app)
+      .post("/auth/logout")
+      .send(fakeMockUserLogin)
+      .expect(200)
+      .end(function(err, res) {
+        done(err)
+      });
+  });
+});
 
 });
+
+
+
+
+
+// ================================================
+  // //User delete
+  // describe('#delete()', function() {
+  //   it('should delete the user', function(done) {
+  //     // request(sails.hooks.http.app)
+  //     //   .post("/auth/login")
+  //     //   .send(fakeMockUserLogin)
+  //     //   .expect(200)
+  //     //   .end(function(err, res) {
+  //         request(sails.hooks.http.app)
+  //         .del("/user")
+  //           .send(fakeMockUserLogin)
+  //           .expect(200)
+  //           .end(function(err, res) {
+  //             done(err)
+  //           });
+  //         // done(err)
+  //       // });
+  //   });
+  //
+  // });
+
+
+
+  // describe('#delete()', function(){
+  //   it('should delete the user', function(done){
+  //     request(sails.hooks.http.app)
+  //     .delete("/user")
+  //     .send(fakeMockUserLogin)
+  //     .expect(200)
+  //     .end(function(err, res) {
+  //       // should(res.body.auth.email).be.exactly(mockUserLogin.email);
+  //       done(err)
+  //     });
+  //   });
+  // });
+
+  // it('should not allow a user to login up providing fake credentials', function (done) {
+  //   request(sails.hooks.http.app)
+  //     .post("/auth/login")
+  //     .send(fakeMockUserLogin)
+  //     .expect(401)
+  //     .end(function(err, res){
+  //       //console.log(err, res)
+  //       done(err)
+  //     });
+  // });
+
+
 
 
 
