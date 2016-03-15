1d5f1d633fcdb04d3ebfa8fd4602f9a6dabca022
andrixb <andrixb@gmail.com>
2016-03-15 23:33:19 +0000

    added more case testing in authcontroller test

--- test/integration/controllers/AuthController.test.js
+++ test/integration/controllers/AuthController.test.js
@@ -1,12 +1,27 @@
 var request = require('supertest');
 var should = require('should');
 
-var users = require("../../fixtures/users.json")
+// var users = require("../../fixtures/users.json")
 
-var mockUserLogin = users[0];
+// var mockUserLogin = users[0];
 
 var fakeMockUserLogin = {
   "email": "John@Armstrong.com",
+  "password": "MoonLanding"
+};
+
+var wrongMockUserLogin = {
+  "email": "John@Armstrong.com",
+  "password": "M00nL2nd1n6"
+};
+
+var wrongMockUserLoginPwd = {
+  "email": "John@Armstrong.com",
+  "password": "M00nL2nd1n6"
+};
+
+var wrongMockUserLoginEmail = {
+  "email": "J0hn@Armstr0n6.c0m",
   "password": "MoonLanding"
 };
 
@@ -30,17 +45,55 @@ describe('AuthController', function() {
           done(err)
         });
     });
+    it('should not allow a user to register not providing all required details', function(done) {
+      request.agent(sails.hooks.http.app)
+        .post("/auth/register")
+        .send(fakeMockUserLogin)
+        .expect(401)
+        .end(function(err, res) {
+          //console.log(err, res)
+          done(err)
+        });
+    });
+
+    it('should not register the same user twice', function(done) {
+      request.agent(sails.hooks.http.app)
+        .post("/auth/register")
+        .send(mockUserRegister)
+        .expect(401)
+        .end(function(err, res) {
+          //console.log(err, res)
+          done(err)
+        });
+    });
   });
 
   // Auth sign/signup/login Page
   describe('#login()', function() {
     it('should allow a user to login up providing  email and pswd', function(done) {
-      request(sails.hooks.http.app)
+      request.agent(sails.hooks.http.app)
         .post("/auth/login")
         .send(fakeMockUserLogin)
         .expect(200)
         .end(function(err, res) {
-          // should(res.body.auth.email).be.exactly(mockUserLogin.email);
+          done(err)
+        });
+    });
+    it('shouldn\'t allow a user to login providing wrong pswd', function(done) {
+      request.agent(sails.hooks.http.app)
+        .post("/auth/login")
+        .send(wrongMockUserLoginPwd)
+        .expect(403)
+        .end(function(err, res) {
+          done(err)
+        });
+    });
+    it('shouldn\'t allow a user to login providing wrong email', function(done) {
+      request.agent(sails.hooks.http.app)
+        .post("/auth/login")
+        .send(wrongMockUserLoginEmail)
+        .expect(403)
+        .end(function(err, res) {
           done(err)
         });
     });
@@ -48,84 +101,20 @@ describe('AuthController', function() {
   });
 
   // Auth logout
-describe('#logout()', function(){
-  it('should logout the user', function(done){
-    request(sails.hooks.http.app)
-      .post("/auth/logout")
-      .send(fakeMockUserLogin)
-      .expect(200)
-      .end(function(err, res) {
-        done(err)
-      });
+  describe('#logout()', function() {
+    it('should logout the user', function(done) {
+      request.agent(sails.hooks.http.app)
+        .post("/auth/logout")
+        .send(fakeMockUserLogin)
+        .expect(200)
+        .end(function(err, res) {
+          done(err)
+        });
+    });
   });
+
+
+
+
+
 });
-
-});
-
-
-
-
-
-// ================================================
-  // //User delete
-  // describe('#delete()', function() {
-  //   it('should delete the user', function(done) {
-  //     // request(sails.hooks.http.app)
-  //     //   .post("/auth/login")
-  //     //   .send(fakeMockUserLogin)
-  //     //   .expect(200)
-  //     //   .end(function(err, res) {
-  //         request(sails.hooks.http.app)
-  //         .del("/user")
-  //           .send(fakeMockUserLogin)
-  //           .expect(200)
-  //           .end(function(err, res) {
-  //             done(err)
-  //           });
-  //         // done(err)
-  //       // });
-  //   });
-  //
-  // });
-
-
-
-  // describe('#delete()', function(){
-  //   it('should delete the user', function(done){
-  //     request(sails.hooks.http.app)
-  //     .delete("/user")
-  //     .send(fakeMockUserLogin)
-  //     .expect(200)
-  //     .end(function(err, res) {
-  //       // should(res.body.auth.email).be.exactly(mockUserLogin.email);
-  //       done(err)
-  //     });
-  //   });
-  // });
-
-  // it('should not allow a user to login up providing fake credentials', function (done) {
-  //   request(sails.hooks.http.app)
-  //     .post("/auth/login")
-  //     .send(fakeMockUserLogin)
-  //     .expect(401)
-  //     .end(function(err, res){
-  //       //console.log(err, res)
-  //       done(err)
-  //     });
-  // });
-
-
-
-
-
-
-// it('should not allow a user to register not providing all required details', function (done) {
-//     request(sails.hooks.http.app)
-//     .post("/auth/register")
-//     .send(mockUserRegister)
-//     .expect(200)
-//     .end(function(err, res){
-//       //console.log(err, res)
-//       done(err)
-//     });
-//   });
--- test/integration/controllers/UserController.test.js
+++ test/integration/controllers/UserController.test.js
@@ -39,3 +39,55 @@
 //
 //
 // });
+
+
+
+
+
+// ================================================
+  // //User delete
+  // describe('#delete()', function() {
+  //   it('should delete the user', function(done) {
+  //     // request(sails.hooks.http.app)
+  //     //   .post("/auth/login")
+  //     //   .send(fakeMockUserLogin)
+  //     //   .expect(200)
+  //     //   .end(function(err, res) {
+  //         request(sails.hooks.http.app)
+  //         .del("/user")
+  //           .send(fakeMockUserLogin)
+  //           .expect(200)
+  //           .end(function(err, res) {
+  //             done(err)
+  //           });
+  //         // done(err)
+  //       // });
+  //   });
+  //
+  // });
+
+
+
+  // describe('#delete()', function(){
+  //   it('should delete the user', function(done){
+  //     request(sails.hooks.http.app)
+  //     .delete("/user")
+  //     .send(fakeMockUserLogin)
+  //     .expect(200)
+  //     .end(function(err, res) {
+  //       // should(res.body.auth.email).be.exactly(mockUserLogin.email);
+  //       done(err)
+  //     });
+  //   });
+  // });
+
+  // it('should not allow a user to login up providing fake credentials', function (done) {
+  //   request(sails.hooks.http.app)
+  //     .post("/auth/login")
+  //     .send(fakeMockUserLogin)
+  //     .expect(401)
+  //     .end(function(err, res){
+  //       //console.log(err, res)
+  //       done(err)
+  //     });
+  // });
