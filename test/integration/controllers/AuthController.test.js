7536cff418cbbe11203ccfb22cd9f542c78f7d61
andrixb <andrixb@gmail.com>
2016-03-15 00:56:08 +0000

    test delete

--- test/integration/controllers/AuthController.test.js
+++ test/integration/controllers/AuthController.test.js
@@ -47,6 +47,44 @@ describe('AuthController', function() {
 
   });
 
+  //User delete
+  describe('#delete()', function() {
+    it('should delete the user', function(done) {
+      request(sails.hooks.http.app)
+        .post("/auth/login")
+        .send(fakeMockUserLogin)
+        .expect(200)
+        .end(function(err, res) {
+          request(sails.hooks.http.app)
+            .del("/user")
+            .send(fakeMockUserLogin)
+            .expect(200)
+            .end(function(err, res) {
+              done(err)
+            });
+
+
+          done(err)
+        });
+    });
+
+  });
+
+
+
+  // describe('#delete()', function(){
+  //   it('should delete the user', function(done){
+  //     request(sails.hooks.http.app)
+  //     .delete("/user")
+  //     .send(fakeMockUserLogin)
+  //     .expect(200)
+  //     .end(function(err, res) {
+  //       // should(res.body.auth.email).be.exactly(mockUserLogin.email);
+  //       done(err)
+  //     });
+  //   });
+  // });
+
   // it('should not allow a user to login up providing fake credentials', function (done) {
   //   request(sails.hooks.http.app)
   //     .post("/auth/login")
@@ -60,19 +98,7 @@ describe('AuthController', function() {
 
 });
 
-// describe('#register()', function() {
-// it('should allow a user to register providing name, lastname, email and pswd', function(done) {
-//   request.agent(sails.hooks.http.app)
-//     .post("/auth/register")
-//     .send(mockUserRegister)
-//     .expect(200)
-//     .end(function(err, res) {
-//       //console.log(err, res)
-//       done(err)
-//     });
-// });
-//
-// });
+
 
 
 // it('should not allow a user to register not providing all required details', function (done) {
